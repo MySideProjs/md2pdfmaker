e190c062b82748000d3b733aa35835eab52499c1
zk <oldsince2017@gmail.com>
2024-11-07 16:27:10 +0800

    feat: add subtitle

--- src/components/app-header/index.tsx
+++ src/components/app-header/index.tsx
@@ -1,7 +1,8 @@
 export const AppHeader = () => {
   return (
-    <h1 className="w-full flex items-center justify-center border-solid border-1 border-t-none border-gray line-height-8rem shadow-md">
-      Markdown to pdf with easy custom style
-    </h1>
+    <div className="border-solid border-1 border-t-none border-gray shadow-md flex items-center flex-col pt-10 pb-10">
+      <h1 className="w-full flex items-center justify-center">Markdown to pdf with easy custom style</h1>
+      <p className="font-semibold">Get what you see</p>
+    </div>
   )
 }
