c6212b779078f3cab2f2f6ca7cff2a0ba56eccfc
zk <oldsince2017@gmail.com>
2024-12-05 10:20:59 +0800

    refact: rm useless deps; rm comment

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -5,15 +5,6 @@ import remarkRehype from "remark-rehype"
 import { unified } from "unified"
 
 export const markdown2html = (md: string) => {
-  const processed = unified()
-    .use(remarkParse)
-    .use(remarkRehype)
-    .use(rehypeSanitize)
-    .use(rehypeStringify)
-    .processSync(md)
-    .value.toString()
-    // .replaceAll("&#x26;", "&")
-  // This is like an patch to handle the remarkRehype plugin's problem
-
+  const processed = unified().use(remarkParse).use(remarkRehype).use(rehypeSanitize).use(rehypeStringify).processSync(md).value.toString()
   return processed
 }
