0b99ce2d009cd19bbff6bc6f3f6941c3cf3ef5c7
zk <oldsince2017@gmail.com>
2025-01-23 14:49:25 +0800

    feat: add fb button back to header buttons

--- src/components/header-buttons/index.tsx
+++ src/components/header-buttons/index.tsx
@@ -1,6 +1,7 @@
 import IconDownload from "@mui/icons-material/Download"
 import IconFolderOpen from "@mui/icons-material/FolderOpen"
 import IconPalette from "@mui/icons-material/Palette"
+import IconFeedback from "@mui/icons-material/Feedback"
 import Fab from "@mui/material/Fab"
 import Button from "@mui/material/Button"
 import { CSSProperties } from "react"
@@ -9,6 +10,7 @@ import { useMarkdownContent } from "../../state"
 import { reportGA } from "../../utils/ga"
 import { useToggle } from "react-use"
 import { StyleDrawer } from "../markdown-preview-style-drawer"
+import { IconButton } from "@mui/material"
 
 export const HeaderButtons = () => {
   const isWide = useIsWide()
@@ -23,6 +25,11 @@ export const HeaderButtons = () => {
   const onFolderOpen = () => {
     loadFileAndOverwriteMarkdownContent()
     reportGA("import_local_md")
+  }
+
+  const onFeedback = () => {
+    window.open("https://forms.gle/God6MvZ6imVXE5hs5")
+    reportGA("open_help_center")
   }
 
   // For mobile
@@ -40,6 +47,10 @@ export const HeaderButtons = () => {
         <Fab onClick={() => toggleStyleDrawer(true)}>
           <IconPalette />
         </Fab>
+        <div className="m-1" />
+        <Fab onClick={onFeedback}>
+          <IconFeedback />
+        </Fab>
         <StyleDrawer open={isStyleDrawerOpen} onClose={() => toggleStyleDrawer(false)} />
       </div>
     )
@@ -54,9 +65,13 @@ export const HeaderButtons = () => {
           Export
         </Button>
 
-        <Button style={{ ...buttonStyle, marginRight: 0 }} variant="contained" startIcon={<IconFolderOpen />} onClick={onFolderOpen}>
+        <Button style={{ ...buttonStyle }} variant="contained" startIcon={<IconFolderOpen />} onClick={onFolderOpen}>
           Import
         </Button>
+
+        <IconButton aria-label="feedback" style={{ ...buttonStyle, marginRight: 0 }} onClick={onFeedback}>
+          <IconFeedback sx={{ color: "white" }} />
+        </IconButton>
       </div>
     </div>
   )
