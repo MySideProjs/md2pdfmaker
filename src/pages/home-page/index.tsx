547f297b0f37e666286ce565e033c9a0130cc6de
zk <oldsince2017@gmail.com>
2025-04-24 13:29:36 +0800

    feat: rm new feat alert

--- src/pages/home-page/index.tsx
+++ src/pages/home-page/index.tsx
@@ -1,18 +1,16 @@
-import { Snackbar, Alert, Button } from "@mui/material"
 import { MarkdownEditorAndPdfViewer } from "../../components/markdown-editor-and-pdf-viewer"
 import { StyleSideBar } from "../../components/markdown-preview-style-sidebar"
 import { useIsWide } from "../../hooks"
-import { useToggle } from "react-use"
 
 export const Home = () => {
   const isWide = useIsWide()
-  const [isSnackbarOn, toggleSnackBar] = useToggle(true)
+  // const [isSnackbarOn, toggleSnackBar] = useToggle(true)
   return (
     <div>
       <MarkdownEditorAndPdfViewer />
       {isWide && <StyleSideBar />}
 
-      {/* New feat snack bar */}
+      {/* New feat snack bar
       <Snackbar open={isSnackbarOn} anchorOrigin={{ vertical: "bottom", horizontal: "center" }}>
         <Alert severity="info" className="flex flex-row items-center">
           <div className="flex flex-row items-center gap-4">
@@ -26,7 +24,7 @@ export const Home = () => {
             </Button>
           </div>
         </Alert>
-      </Snackbar>
+      </Snackbar> */}
     </div>
   )
 }
